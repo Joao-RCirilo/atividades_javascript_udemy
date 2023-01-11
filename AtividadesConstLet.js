54c727b9537c12a2e5548e451cbcdb945824e50c
João Ricardo <80233584+Joao-RCirilo@users.noreply.github.com>
2023-01-11 10:08:33 -0300

    exercício realizado

--- AtividadesConstLet.js
+++ AtividadesConstLet.js
@@ -1,1 +1,22 @@
+/* 
+João Ricardo tem 24 anos, pesa 68 kg tem 1,72 metros de altura e seu imc é de: 
+João Ricardo nasceu em 1998
+*/
 
+const nome = 'João Ricardo';
+const segundonome = 'Ricardo';
+const idade = 24;
+const peso = 68;
+const altura = 1.72;
+let imc; peso/(altura * altura)
+let anoNascimento;
+
+
+imc = peso/(altura*altura);
+anoNascimento = 2022 - idade;
+
+console.log(nome, segundonome, ' tem ,', idade, 'pesa ', peso, ' tem ', altura, ' e seu imc é de ', imc, nome, segundonome, ' em ', anoNascimento );
+
+console.log(nome + segundonome + ' tem ,'+ idade, 'pesa ' + peso + ' tem ' + altura + ' e seu imc é de ' + imc + nome + segundonome + ' em ' + anoNascimento );
+
+console.log(`${nome + segundonome}, ' tem ,'${idade}, 'pesa ', ${peso}, ' tem ', ${altura}, ' e seu imc é de ', ${imc}, ${nome}, ${segundonome}, ' em ', ${anoNascimento}` );
